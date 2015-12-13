05129abd5bba8731cffab01c113787368521569d
costa-amore <costa.amore@gmail.com>
2015-12-13 15:24:32 +0100

    Setup for AAA style specs : Test classes inherting from this baseclass, can override the   *  Arrange (to setup the testcase),   *  Act (to trigger the behavior) methods to set the context for a specific test case.
    They are usually named like : When<triggering><Context>.
    
    The [Test] methods then assert each specific aspect of the behavior in the given context.
    
    When logging the testresults, you get something like:
     * WhenCalcualtingNothing
        * ItReturnsString
        * ItReturnsZero

--- Core.Tests/Test.cs
+++ Core.Tests/Test.cs
@@ -1,10 +1,11 @@
 ﻿using NUnit.Framework;
 using System;
+using AEGIS.Specs.Framework;
 
 namespace Core.Tests
 {
 	[TestFixture ()]
-	public class Test
+	public class WhenCalculatingNothing: AaaStyleSpec
 	{
 		[Test ()]
 		public void TestCase ()
