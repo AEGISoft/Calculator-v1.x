8a79fdb6c815c9ba0f0baf8a5d877bcb12a5165f
costa-amore <costa.amore@gmail.com>
2015-12-13 16:16:30 +0100

    Added 2nd test

---
+++ Core.Tests/IntegerCalculations/WhenCalculatingSingleInteger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AEGIS.Specs.Framework;
+using NUnit.Framework;
+using AEGIS.Calculator.Core;
+
+namespace AEGIS.Calculator.Core.Tests
+{
+	public class WhenCalculatingSingleInteger:AaaStyleSpec
+	{
+		Calculator _calculator;
+		String _inputInteger;
+
+		protected override void Arrange(){
+			_calculator = new IntegerCalculator();
+			_inputInteger = "123";
+		}
+
+		protected override void Act(){
+			_calculator.Calculate(_inputInteger);
+		}
+
+		[Test]
+		public void ItReturnsTheInputInteger(){
+			Assert.That(_calculator.Result, Is.EqualTo(_inputInteger));
+		}
+	}
+}
+
