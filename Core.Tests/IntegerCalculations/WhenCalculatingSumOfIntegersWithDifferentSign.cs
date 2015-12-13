3057cbbea9a86afd81098bb7a35533b40ebea9ea
costa-amore <costa.amore@gmail.com>
2015-12-13 18:30:01 +0100

    Test for integers of different sign succeed immediately

---
+++ Core.Tests/IntegerCalculations/WhenCalculatingSumOfIntegersWithDifferentSign.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AEGIS.Specs.Framework;
+using NUnit.Framework;
+
+namespace AEGIS.Calculator.Core.Tests.IntegerCalculations
+{
+	public class WhenCalculatingSumOfIntegersWithDifferentSign: AaaStyleSpec
+
+	{
+		Common.Calculator _calculator;
+		String _input;
+		String _expectedResult;
+
+		protected override void Arrange ()
+		{
+			_calculator = new IntegerCalculator();
+			_input = "-1 + 5";
+			_expectedResult = "4";
+		}
+
+		protected override void Act ()
+		{
+			_calculator.Calculate(_input);
+		}
+
+		[Test]
+		public void ItReturnsTheSum(){
+			Assert.That (_calculator.Result, Is.EqualTo(_expectedResult));
+		}
+	}
+}
+
