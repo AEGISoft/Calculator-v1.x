7fe9e85edd58b4726fa44a8efeee9c599e9b9330
costa-amore <costa.amore@gmail.com>
2015-12-13 18:56:56 +0100

    added failing test for adding sum of multiple integers with different sign

---
+++ Core.Tests/IntegerCalculations/WhenCalculatingSumOfMultipleIntegersOfDifferentSign.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AEGIS.Calculator.Core.Tests.IntegerCalculations;
+using NUnit.Framework;
+
+namespace AEGIS.Calculator.Core.Tests
+{
+	public class WhenCalculatingSumOfMultipleIntegersOfDifferentSign: AaaStyleIntegerCalculationTest
+	{
+		protected override void Arrange ()
+		{
+			base.Arrange ();
+			_input = "1 - 5 + 6";
+			_expectedResult = "2";
+		}
+
+		[Test]
+		public void ItReturnsTheSum()
+		{
+			Assert.That(_calculator.Result, Is.EqualTo(_expectedResult));
+		}
+	}
+}
+
